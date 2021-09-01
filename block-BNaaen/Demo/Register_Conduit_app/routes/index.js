901a51d7f41817e9db35fe4b5b29a27175ea9014
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-09-01 22:46:18 +0530

    modify

--- block-BNaaen/Demo/Register_Conduit_app/routes/index.js
+++ block-BNaaen/Demo/Register_Conduit_app/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 let Article = require('../models/articleModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+module.exports = router;
--- block-BNaaen/Demo/Register_Conduit_app/routes/tags.js
+++ block-BNaaen/Demo/Register_Conduit_app/routes/tags.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let router = express.Router();
-
+let Article = require('../models/articleModel');
 //Get Tags
 router.get('/', async (req, res, next) => {
+  console.log('enetered');
   try {
     let tags = await Article.find({}).distinct('tagList');
     res.status(200).json({ tags });
